c2398d826106d2f3746eaedcc978a657df51a717
Benjamin Ben Rachmiel <benjamin@benrachmiel.org>
2026-03-28 21:21:22 +0000

    Fixing background and pwa extending into safe areas

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,9 +25,10 @@ export const metadata: Metadata = {
 };
 
 export const viewport: Viewport = {
-  themeColor: "#09090b",
+  themeColor: "#000000",
   width: "device-width",
   initialScale: 1,
+  viewportFit: "cover",
 };
 
 export default function RootLayout({
